a8a95dd7e4de8c5bd60179ce7a26df7cdbf8810f
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-01-27 18:41:16 +0530

    Added Register and Signin methods

--- Server/route/auth.js
+++ Server/route/auth.js
@@ -1,16 +1,90 @@
 const express = require('express');
 const router = express.Router();
 
+//Database Connection
+require("../db/connection");
+//userRegister 
+const UserRegister = require("../model/registerSchema");
+
 router.get('/', (req, res)=>{
     res.send('Server is Up!')
 });
 
+//Using promise
 // Route for registering user
-router.post('/register', (req, res) =>{
-    console.log(req.body);
-    // res.send('my router page')
-    res.json({message: req.body}); 
+// router.post('/register', (req, res) =>{
+    
+//     const { name, email, password, confirmPassword } = req.body ;
+    
+//     if( !name || !email || !password || !confirmPassword){
+//         return res.status(422).json({ error: "Please Fill the details properly"});
+//     }
+
+//     UserRegister.findOne({email: email})
+//     .then((userExist) =>{
+//         if(userExist){
+//             return res.status(422).json({ error: "Email already Exist"});
+//         }
+
+//         const user = new UserRegister({ name, email, password, confirmPassword });
+
+//         user.save().then(()=>{
+//             res.status(201).json({message: "User registered successfuly"});
+//         }).catch((err)=> res.status(500).json({error:"Failed to register"}));
+//     }).catch( err =>{console.log(err)});
+
+// });
+
+//Using async and await
+
+router.post('/register', async (req, res) =>{
+    
+    const { name, email, password, confirmPassword } = req.body ;
+    
+    if( !name || !email || !password || !confirmPassword){
+        return res.status(422).json({ error: "Please Fill the details properly"});
+    }
+
+    try{
+        const userExist = await UserRegister.findOne({email: email});
+
+        if(userExist){
+            return res.status(422).json({ error: "Email already Exist"});
+        }
+    
+        const user = new UserRegister({ name, email, password, confirmPassword });
+
+        await user.save();
+        res.status(201).json({message: "User Registered Successfuly"});
+        
+    } catch(err){
+        console.log(err);
+    }
 });
+
+// Login route
+router.post('/signin', async (req, res)=>{
+    try{
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({error:"Pls fill the required details"});
+        }
+
+        const userLogin = await UserRegister.findOne({ email: email});
+        console.log(userLogin);
+
+        if(!userLogin){
+            res.status(400).json({ error:"User error" });
+        }else{
+            res.json({ message:"User SignedIn Successfully" });
+        }
+
+    }catch(err){
+        console.log(err);
+    }
+});
+
 
 
 module.exports = router;
