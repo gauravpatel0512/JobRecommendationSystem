0d9e483f742a728dc8fbfc74e8983eb7ff862fcb
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-01-31 20:49:51 +0530

    Added Password hashing and Method for generating Token using JWT

--- Server/model/registerSchema.js
+++ Server/model/registerSchema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const registerSchema = new mongoose.Schema({
    name:{
@@ -16,9 +18,40 @@ const registerSchema = new mongoose.Schema({
    confirmPassword: {
       type: String,
       required: true
-   }
+   },
+   tokens: [
+      {
+         token:{
+            type: String,
+            required: true
+         }
+      }
+   ]
 })
 
+
+// Hashing the password and confirmPassword
+registerSchema.pre('save', async function(next) {
+   if(this.isModified('password')){
+      this.password = await bcrypt.hash(this.password, 12);
+      this.confirmPassword = await bcrypt.hash(this.confirmPassword, 12);
+   }
+   next();
+});
+// generating token
+registerSchema.methods.generateAuthToken = async function(){
+   try{
+      let token = jwt.sign({_id: this._id}, process.env.SECERET_KEY);
+      this.tokens = this.tokens.concat({token: token});
+      await this.save();
+      return token;
+   }catch( err ){
+      console.log(err);
+   }
+};
+
+// collection creation
 const UserRegister = mongoose.model('USERREGISTER' , registerSchema);
 
 module.exports = UserRegister;
+
