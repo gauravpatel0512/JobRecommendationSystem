a393d34eae996bb55cc51a79f80265f515283ed0
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-03-08 09:23:04 +0530

    created new routes

--- Server/route/dashboard.js
+++ Server/route/dashboard.js
@@ -5,12 +5,18 @@ require("../db/connection");
 //Connecting Mongodb using MongoClient
 // const { MongoClient } = require("mongodb");
 // const client = new MongoClient(process.env.DATABASE);
+//Schema
 const DashboardSchema = require("../model/companySchema");
 
-
-router.get("/dashboard", async (req, res, next) => {
-	console.log("Dashboard is UP!");
-	res.send("Dashboard is UP!");
+router.get("/dashboard/:jobId", async (req, res, next) => {
+	try {
+		const data = await DashboardSchema.findOne({ jobId: req.params.jobId });
+		res.send(data);
+		console.log("Job Post found");
+	} catch {
+		res.status(404);
+		res.send({ error: "Job post doesn't exist!" });
+	}
 });
 
 module.exports = router;
