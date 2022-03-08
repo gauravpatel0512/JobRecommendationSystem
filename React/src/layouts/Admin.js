cffa56a93e2ad60929051e0adaa3ae097c8ba91d
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-03-08 09:19:36 +0530

    Initial commit

--- React/src/layouts/Admin.js
+++ React/src/layouts/Admin.js
@@ -16,24 +16,23 @@ import Resume from "views/admin/Resume.js";
 import Tables from "views/admin/Tables.js";
 
 export default function Admin() {
-  return (
-    <>
-      <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100">
-        <AdminNavbar />
-        {/* Header */}
-        {/* <HeaderStats /> */}
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
-          <Switch>
-            <Route path="/admin/dashboard" exact component={Dashboard} />
-            {/* <Route path="/admin/maps" exact component={Maps} /> */}
-            <Route path="/admin/resume" exact component={Resume} />
-            <Route path="/admin/tables" exact component={Tables} />
-            <Redirect from="/admin" to="/admin/dashboard" />
-          </Switch>
-          {/* <FooterAdmin /> */}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Sidebar />
+			<div className="relative md:ml-64 ">
+				{/* Header */}
+				{/* <HeaderStats /> */}
+				<div className="px-4 md:px-10  mx-auto  -m-24">
+					<Switch>
+						<Route path="/admin/dashboard" exact component={Dashboard} />
+						{/* <Route path="/admin/maps" exact component={Maps} /> */}
+						<Route path="/admin/resume" exact component={Resume} />
+						<Route path="/admin/tables" exact component={Tables} />
+						<Redirect from="/admin" to="/admin/dashboard" />
+					</Switch>
+					{/* <FooterAdmin /> */}
+				</div>
+			</div>
+		</>
+	);
 }
