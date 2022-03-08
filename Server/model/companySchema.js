39363de925934749bbc3bb382f62f4af5e8a9dc5
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-03-08 09:22:24 +0530

    Created company schema

--- Server/model/companySchema.js
+++ Server/model/companySchema.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 
 const companySchema = new mongoose.Schema({
-	id: String,
-	advertiserurl: String,
-	company: String,
-	employmenttype_jobstatus: String,
-	jobdescription: String,
-	jobid: String,
-	joblocation_address: String,
-	jobtitle: String,
-	postdate: String,
-	shift: String,
-	skills: String,
-	uniq_id: String,
+	jobId: String,
+	jobTitle: String,
+	companyName: String,
+	location: String,
+	jobType: String,
+	description: String,
 });
 
 const DashboardSchema = mongoose.model("dice_com_job_us_sample", companySchema);
