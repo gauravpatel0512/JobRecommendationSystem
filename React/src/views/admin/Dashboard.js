8955fd41cc5ee81cd7e97553f4879fb7e71ef92e
GauravPatel <69468357+gauravpatel0512@users.noreply.github.com>
2022-03-29 11:29:28 +0530

    Changed Dashboard UI

--- React/src/views/admin/Dashboard.js
+++ React/src/views/admin/Dashboard.js
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Grid from "./Grid";
 // import { Wrapper, Content } from "./Dashboard.styles";
 // components
 
 import CardData from "components/Cards/CardData.js";
-import CardBarChart from "components/Cards/CardBarChart.js";
-import CardPageVisits from "components/Cards/CardPageVisits.js";
-import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+// import data from "../../assets/companyData/dice_com_job_us_samples.json";
+// import CardBarChart from "components/Cards/CardBarChart.js";
+// import CardPageVisits from "components/Cards/CardPageVisits.js";
+// import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 
 export default function Dashboard() {
-	const [dashboardData, setDashboardData] = useState("");
+	const [dashboardData, setDashboardData] = useState([]);
+
+	useEffect(() => {
+		fetch("/dashboard")
+			.then((res) => res.json())
+			.then((data) => {
+				setDashboardData(data);
+			});
+	}, []);
+	console.log(dashboardData);
+
 	return (
 		<div className=" pt-32 xl:w-8/12 mb-12 ">
-			<CardData />
+			<button className="mb-3 bg-lightBlue-500  text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150">
+				Refresh
+			</button>
+			{dashboardData.map((job) => (
+				<CardData job={job} key={job.jobId} />
+			))}
+			{/* <CardData /> */}
 		</div>
 
 		// <div className=" pt-48">
